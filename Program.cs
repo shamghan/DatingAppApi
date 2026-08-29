d076bd5d52663fe85330cce058714ed77bc977d1
GHANSHAM KASHYUP <66261311+shamghan@users.noreply.github.com>
2026-08-29 23:58:06 +0530

    Implement Member repository and and Member Controller

--- Program.cs
+++ Program.cs
@@ -20,6 +20,7 @@ builder.Services.AddDbContext<AppDbContext>(option=>
 
 builder.Services.AddCors();
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IMemberRepository, MemberRepository>();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
