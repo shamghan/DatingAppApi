cdf0f05e578532879bbd6b8ed0a7a2660d403ea5
GHANSHAM KASHYUP <66261311+shamghan@users.noreply.github.com>
2026-09-13 14:04:31 +0530

    set utc  time zone

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using DatingApp.Entities;
 using DatingAppApi.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DatingApp.Data
 {
@@ -10,5 +11,28 @@ namespace DatingApp.Data
 
         public DbSet<Member> Members { get; set;}
         public DbSet<Photo> Photos { get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var dateTimeConverter = new ValueConverter<DateTime,DateTime>(
+                v=> v.ToUniversalTime(),
+                v=> DateTime.SpecifyKind(v, DateTimeKind.Utc)
+
+            );
+
+            foreach(var enttyTyp in modelBuilder.Model.GetEntityTypes())
+            {
+                foreach(var property in enttyTyp.GetProperties())
+                {
+                    if(property.ClrType == typeof(DateTime))
+                    {
+                        property.SetValueConverter(dateTimeConverter);
+                    }
+                }
+            }
+        }
+
     }
 }
