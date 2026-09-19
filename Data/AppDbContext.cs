c9b9a411b82cf958f2f6c6fd791dd03d10d5963c
GHANSHAM KASHYUP <66261311+shamghan@users.noreply.github.com>
2026-09-19 23:14:37 +0530

    action filter pending code commit

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -5,16 +5,31 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DatingApp.Data
 {
-    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+   public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
         public DbSet<AppUser> Users { get; set; }
 
         public DbSet<Member> Members { get; set;}
         public DbSet<Photo> Photos { get; set;}
+        public DbSet<MemberLike> Likes { get; set;}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<MemberLike>()
+                .HasKey(e => new {e.SourceMemberId, e.TargetMemberid});
+
+            modelBuilder.Entity<MemberLike>()
+            .HasOne(e => e.SourceMember)
+            .WithMany(e => e.LikedMembers)
+            .HasForeignKey(e => e.SourceMemberId)
+            .OnDelete(DeleteBehavior.Cascade);
+            
+            modelBuilder.Entity<MemberLike>()
+            .HasOne(e => e.TargetMember)
+            .WithMany(e => e.LikedByMembers)
+            .HasForeignKey(e => e.TargetMemberid)
+            .OnDelete(DeleteBehavior.NoAction);
 
             var dateTimeConverter = new ValueConverter<DateTime,DateTime>(
                 v=> v.ToUniversalTime(),
